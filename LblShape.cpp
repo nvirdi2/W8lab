20e5f47f5dcd044e39a18c8bcf450ec0340235b4
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-25 18:37:54 -0400

    Update LblShape.cpp

--- LblShape.cpp
+++ LblShape.cpp
@@ -6,9 +6,12 @@
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
 #define _CRT_SECURE_NO_WARNINGS
+
 #include "LblShape.h"
 #include <cstring>
+
 #include <string>
+
 using namespace std;
 
 namespace sdds
